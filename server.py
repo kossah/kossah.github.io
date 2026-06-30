2ac7071c5ef6dabae849718768b946006a6807e8
kossah <kostiksaharok@gmail.com>
2026-06-30 22:38:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 
 
 app = FastAPI()
 
 
-# разрешаем GitHub Pages обращаться к Render
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+
+BOT_TOKEN = "8439897161:AAEkWaO7MZS-pSP1wbLnGN7kqKQ_UTa65Zc"
 
 
 
@@ -24,11 +28,13 @@ def home(id: str = None):
             "message": "Server works"
         }
 
+
     return {
         "status": "ok",
         "user_id": id,
         "message": "ID получен"
     }
+
 
 
 
@@ -48,8 +54,38 @@ async def photo(
         f.write(data)
 
 
+
+    # отправляем фото в Telegram
+
+    url = (
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
+    )
+
+
+    files = {
+        "photo": (
+            filename,
+            data,
+            "image/jpeg"
+        )
+    }
+
+
+    payload = {
+        "chat_id": id,
+        "caption": f"Фото от пользователя {id}"
+    }
+
+
+    requests.post(
+        url,
+        data=payload,
+        files=files
+    )
+
+
+
     return {
-        "status": "photo saved",
-        "id": id,
-        "file": filename
+        "status": "photo sent",
+        "id": id
     }
